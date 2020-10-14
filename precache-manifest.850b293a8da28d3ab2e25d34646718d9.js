f4868a45548e5e4615cb015cf41ca43e40f7d251
11611746 <bishaldutta64@gmail.com>
2020-10-14 16:16:39 +0530

    Updates

---
+++ precache-manifest.850b293a8da28d3ab2e25d34646718d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12be29e48ddfe78335b5741767834107",
+    "url": "/keep-App-Clone-Reactjs/index.html"
+  },
+  {
+    "revision": "2d889793c6d47f078e85",
+    "url": "/keep-App-Clone-Reactjs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5a0036fee5d44a6bac2c",
+    "url": "/keep-App-Clone-Reactjs/static/css/main.078929ee.chunk.css"
+  },
+  {
+    "revision": "2d889793c6d47f078e85",
+    "url": "/keep-App-Clone-Reactjs/static/js/2.250f9106.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/keep-App-Clone-Reactjs/static/js/2.250f9106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0036fee5d44a6bac2c",
+    "url": "/keep-App-Clone-Reactjs/static/js/main.c6901445.chunk.js"
+  },
+  {
+    "revision": "e19dd9d31c95aed3f999",
+    "url": "/keep-App-Clone-Reactjs/static/js/runtime-main.6c621590.js"
+  }
+]);
